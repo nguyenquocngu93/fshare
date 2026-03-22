e13b6fb4267c69906322372eca30b3d384d91d52
nguyenquocngu93 <158430142+nguyenquocngu93@users.noreply.github.com>
2026-03-22 18:56:39 +0700

    Update kkphim.js

--- kkphim.js
+++ kkphim.js
@@ -13,8 +13,8 @@
     Lampa.Component.add('kkphim_catalog', function () {
         var network = new Lampa.Reguest();
         var scroll  = new Lampa.Scroll({ mask: true, over: true });
-        var page    = 1;
         var html    = $('<div class="kkphim-wrap"></div>');
+        var page    = 1;
 
         this.create = function () {
             this.activity.loader(true);
@@ -53,27 +53,54 @@
     });
 
     // ==========================================
-    // THÊM VÀO MENU - CÁCH ĐÚNG
+    // INJECT VÀO MENU
     // ==========================================
-    function addMenu() {
-        Lampa.Listener.follow('menu', function (e) {
-            if (e.type === 'start') {
-                e.items.push({
-                    title:     'KKPhim',
-                    component: 'kkphim_catalog',
-                    icon:      Lampa.SVG ? Lampa.SVG.icon('cinema') : '',
-                });
-            }
+    function addMenuItem() {
+        var item = $([
+            '<li class="menu__item selector" data-action="kkphim_catalog">',
+            '    <div class="menu__ico">',
+            '        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">',
+            '            <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>',
+            '        </svg>',
+            '    </div>',
+            '    <div class="menu__text">KKPhim</div>',
+            '</li>'
+        ].join(''));
+
+        item.on('hover:enter click', function () {
+            Lampa.Activity.push({
+                url:       '',
+                title:     'KKPhim',
+                component: 'kkphim_catalog',
+                page:      1,
+            });
         });
+
+        var menuList = $('.menu .menu__list').first();
+        if (menuList.length) {
+            menuList.append(item);
+        }
+    }
+
+    function init() {
+        var tries = 0;
+        var timer = setInterval(function () {
+            tries++;
+            var menuList = $('.menu .menu__list').first();
+            if (menuList.length) {
+                clearInterval(timer);
+                addMenuItem();
+            }
+            if (tries > 20) clearInterval(timer);
+        }, 500);
     }
 
     if (window.appready) {
-        addMenu();
+        init();
     } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') addMenu();
+            if (e.type === 'ready') init();
         });
     }
 
 })();
-
