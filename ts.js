048f87eaab59ca945bef0611fca1d597aa24edce
nguyenquocngu93 <158430142+nguyenquocngu93@users.noreply.github.com>
2026-03-25 00:48:46 +0700

    Update ts.js

--- ts.js
+++ ts.js
@@ -1,184 +1,110 @@
-(function () {
+(function(){
+
 'use strict';
 
-if (window.lampa_torrent_plugin_ready) return;
-window.lampa_torrent_plugin_ready = true;
-
-console.log('Torrent Plugin Loaded');
+if(window.torrent_plugin_v8) return;
+window.torrent_plugin_v8 = true;
 
 var TORRENTIO = 'https://torrentio.strem.fun/sort=size/stream';
-var TORRSERVER = 'http://gren439e.tsarea.tv:8880';
 
-function request(url, success, error){
+function getImdb(card){
 
-var r = new Lampa.Reguest();
-r.timeout(15000);
+return card.imdb_id ||
+(card.ids && card.ids.imdb) ||
+(card.external_ids && card.external_ids.imdb_id);
 
-r.silent(url,function(data){
-success(data);
-},function(a,b){
-error(a);
+}
+
+function tsUrl(){
+
+return 'http://gren439e.tsarea.tv:8880';
+
+}
+
+function play(hash,index,title){
+
+var url = tsUrl() + '/stream?link=' + hash + '&index=' + index + '&play';
+
+Lampa.Player.play({
+title:title,
+url:url
 });
 
 }
 
-function parseSize(size){
+function parseSize(s){
 
-if(!size) return 0;
+if(!s) return 0;
 
-var m = size.match(/([\d.]+)\s*(GB|MB|TB)/i);
+var m = s.match(/([\d\.]+)\s*(GB|MB|TB)/i);
 
 if(!m) return 0;
 
 var n = parseFloat(m[1]);
 
-switch(m[2].toUpperCase()){
+var u = m[2].toUpperCase();
 
-case 'TB': return n*1000*1000*1000*1000;
-case 'GB': return n*1000*1000*1000;
-case 'MB': return n*1000*1000;
-
-}
+if(u=='TB') n*=1000;
+if(u=='GB') n*=1;
+if(u=='MB') n/=1000;
 
 return n;
 
 }
 
-function getIMDB(card){
+function search(card){
 
-var id = card.imdb_id || '';
+var imdb = getImdb(card);
 
-if(!id && card.ids) id = card.ids.imdb;
+if(!imdb){
 
-if(!id && card.external_ids) id = card.external_ids.imdb_id;
-
-if(id && !/^tt/.test(id)) id = 'tt'+id;
-
-return id;
-
-}
-
-function getType(card){
-
-if(card.type=='tv') return 'series';
-
-if(card.name && card.first_air_date) return 'series';
-
-return 'movie';
-
-}
-
-function fetchTorrent(imdb,type,callback){
-
-var url;
-
-if(type=='movie'){
-
-url = TORRENTIO+'/movie/'+imdb+'.json';
-
-}else{
-
-url = TORRENTIO+'/series/'+imdb+':1:1.json';
-
-}
-
-request(url,function(data){
-
-var streams = data.streams || [];
-
-var list = [];
-
-streams.forEach(function(s){
-
-if(!s.infoHash) return;
-
-var lines = (s.title||'').split('\n');
-
-var name = lines[0] || 'Torrent';
-
-var info = lines[1] || '';
-
-var sizeMatch = info.match(/💾\s*(.*)/);
-
-var size = sizeMatch ? sizeMatch[1] : '';
-
-list.push({
-
-title:name,
-size:size,
-sizeNum:parseSize(size),
-hash:s.infoHash,
-fileIdx:s.fileIdx || 0
-
-});
-
-});
-
-list.sort(function(a,b){
-return b.sizeNum - a.sizeNum;
-});
-
-callback(list);
-
-},function(){
-
-Lampa.Noty.show('Torrentio error');
-
-callback([]);
-
-});
-
-}
-
-function playTorrent(hash,index,title){
-
-request(TORRSERVER+'/torrent/stat?hash='+hash,function(data){
-
-var files = data.Files || [];
-
-var video = files
-.filter(function(f){
-
-return /\.(mkv|mp4|avi|ts|m2ts|mov)$/i.test(f.Name);
-
-})
-.sort(function(a,b){
-
-return b.Size-a.Size;
-
-})[0];
-
-if(!video){
-
-Lampa.Noty.show('No video file');
-
+Lampa.Noty.show('Không có IMDB');
 return;
 
 }
 
-var url = TORRSERVER+'/stream?link='+hash+'&index='+video.Id+'&play';
+Lampa.Noty.show('Searching torrent...');
 
-Lampa.Player.play({
+var url = TORRENTIO + '/movie/' + imdb + '.json';
 
-url:url,
-title:title
+new Lampa.Reguest().silent(url,function(data){
+
+var arr = (data.streams || []).map(function(s){
+
+var lines = (s.title||'').split('\n');
+
+var sizeMatch = (lines[1]||'').match(/💾\s*(.+)/);
+
+var size = sizeMatch ? sizeMatch[1] : '';
+
+return {
+
+title:lines[0],
+size:size,
+sizeNum:parseSize(size),
+seeds:(lines[1]||'').match(/👤\s*(\d+)/)?.[1]||0,
+hash:s.infoHash,
+index:s.fileIdx || 0
+
+};
 
 });
 
-},function(){
+arr.sort(function(a,b){
+return b.sizeNum - a.sizeNum;
+});
 
-Lampa.Noty.show('Torrent load error');
+show(arr,card.title);
 
 });
 
 }
 
-function showList(list){
+function show(list,title){
 
 if(!list.length){
 
 Lampa.Noty.show('No torrent found');
-
 return;
 
 }
@@ -189,10 +115,10 @@ title:'Torrent',
 
 items:list.map(function(t){
 
-return{
+return {
 
 title:t.title,
-subtitle:t.size,
+subtitle:'👤 '+t.seeds+' 💾 '+t.size,
 torrent:t
 
 };
@@ -201,35 +127,9 @@ torrent:t
 
 onSelect:function(a){
 
-var t = a.torrent;
-
-playTorrent(t.hash,t.fileIdx,t.title);
+play(a.torrent.hash,a.torrent.index,a.torrent.title);
 
 }
-
-});
-
-}
-
-function startSearch(card){
-
-var imdb = getIMDB(card);
-
-if(!imdb){
-
-Lampa.Noty.show('No IMDB');
-
-return;
-
-}
-
-var type = getType(card);
-
-Lampa.Noty.show('Searching torrent...');
-
-fetchTorrent(imdb,type,function(list){
-
-showList(list);
 
 });
 
@@ -243,30 +143,32 @@ var card = e.data.movie;
 
 if(!card) return;
 
-var btn = {
+setTimeout(function(){
 
-title:'Torrent',
+var buttons = document.querySelector('.full-start__buttons');
 
-onSelect:function(){
+if(!buttons) return;
 
-startSearch(card);
+if(document.querySelector('.torrent-btn')) return;
 
-}
+var btn = document.createElement('div');
+
+btn.className='full-start__button selector torrent-btn';
+
+btn.innerHTML='<span>Torrent</span>';
+
+btn.onclick=function(){
+
+search(card);
 
 };
 
-Lampa.Activity.push({
+buttons.appendChild(btn);
 
-title:'Torrent',
-component:'empty',
-onStart:function(){
-
-startSearch(card);
-
-}
+},500);
 
 });
 
-});
+console.log('Torrent Plugin v8 loaded');
 
 })();
