cd82a57c6ac39f848174b9dd1ef6b9ddda5cac3d
nguyenquocngu93 <158430142+nguyenquocngu93@users.noreply.github.com>
2026-03-24 21:15:04 +0700

    Update ts.js

--- ts.js
+++ ts.js
@@ -1,109 +1,86 @@
 (function () {
     'use strict';
 
-    function TorrentioJackettStyle() {
-        var server = 'http://gren439e.tsarea.tv:8880';
+    function Jackett(object) {
+        var network = new Lampa.RegExp();
+        var scroll = new Lampa.Scroll({mask: true, over: true});
+        var items = [];
+        var html = $('<div></div>');
+        var body = $('<div class="category-full"></div>');
+        
+        // --- ĐOẠN MOD: THIẾT LẬP SERVER JACRED ---
+        var jack_url = 'https://jac.maxvol.pro';
+        var jack_key = '1';
+        // ---------------------------------------
 
-        // Tạo Component để hiển thị kết quả giống Jackett
-        Lampa.Component.add('torrentio_mod', function (object, str) {
-            var network = new Lampa.Reguest();
-            var scroll = new Lampa.Scroll({mask: true, over: true});
-            var files = new Lampa.Files(object);
-            var html = $('<div></div>');
-            
-            this.create = function () {
-                var _this = this;
-                this.search();
-                return scroll.render();
-            };
+        this.create = function () {
+            var _this = this;
+            this.start();
+            return this.render();
+        };
 
-            this.search = function () {
-                var _this = this;
-                var movie = object.movie;
-                var type = movie.number_of_seasons ? 'series' : 'movie';
-                var s = Lampa.Storage.get('online_last_season_' + movie.id, 1);
-                var ep = Lampa.Storage.get('online_last_episode_' + movie.id, 1);
-                var id = type === 'series' ? `${movie.imdb_id}:${s}:${ep}` : movie.imdb_id;
+        this.start = function () {
+            var _this = this;
+            // Tạo URL tìm kiếm chuẩn cho Jacred API v1
+            var url = jack_url + '/api/v1/search?api_key=' + jack_key + '&query=' + encodeURIComponent(object.search);
 
-                network.silent(`https://torrentio.strem.io/streams/${type}/${id}.json`, function (json) {
-                    if (json.streams && json.streams.length > 0) {
-                        _this.build(json.streams);
-                    } else {
-                        Lampa.Noty.show('Không tìm thấy link');
-                    }
-                });
-            };
-
-            this.build = function (streams) {
-                var _this = this;
-                // Sắp xếp theo dung lượng (Logic từ Jackett)
-                streams.sort(function(a, b) {
-                    var sizeA = a.title.match(/💾\s*([\d.]+\s*[GB|MB]+)/);
-                    var sizeB = b.title.match(/💾\s*([\d.]+\s*[GB|MB]+)/);
-                    return (sizeB ? parseFloat(sizeB[1]) : 0) - (sizeA ? parseFloat(sizeA[1]) : 0);
-                });
-
-                streams.forEach(function (stream) {
-                    var lines = stream.title.split('\n');
-                    var item = Lampa.Template.get('button', {title: lines[0], subtitle: lines[1]});
-                    
-                    item.on('hover:enter', function () {
-                        var magnet = 'magnet:?xt=urn:btih:' + stream.infoHash;
-                        var url = server + '/stream/magnet?link=' + encodeURIComponent(magnet) + '&play';
-                        Lampa.Player.play({
-                            url: url,
-                            title: object.movie.title,
-                            subtitle: lines[0]
-                        });
-                    });
-                    html.append(item);
-                });
-                scroll.append(html);
-                _this.checkState();
-            };
-
-            this.checkState = function(){
-                Lampa.Controller.enable('content');
-            };
-
-            this.render = function () { return scroll.render(); };
-            this.pause = function () {};
-            this.stop = function () {};
-        });
-
-        // CHÈN NÚT VÀO TRANG FULL (GIỐNG JACKETT)
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type == 'complite') {
-                var btn = $(`<div class="full-start__button selector">
-                    <svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z" fill="white"/></svg>
-                    <span>Torrentio</span>
-                </div>`);
-
-                btn.on('hover:enter', function () {
-                    Lampa.Component.item({
-                        type: 'torrentio_mod',
-                        title: 'Torrentio',
-                        movie: e.data.movie,
-                        id: e.data.movie.id
-                    });
-                });
-
-                // Jackett dùng cách chèn này để đảm bảo nút hiện lên
-                var target = e.container.find('.full-start__buttons');
-                if (target.length) {
-                    target.append(btn);
-                    // Ép Controller cập nhật lại để nhận nút
-                    Lampa.Controller.add('full_start', {
-                        toggle: function () {
-                            Lampa.Controller.collectionSet(e.container);
-                            Lampa.Controller.render();
-                        }
-                    });
+            Lampa.Network.silent(url, function (json) {
+                if (json && json.result && json.result.length) {
+                    _this.build(json.result);
+                } else {
+                    _this.empty();
                 }
-            }
-        });
+            }, function () {
+                _this.empty();
+            });
+        };
+
+        this.build = function (data) {
+            var _this = this;
+            data.forEach(function (item) {
+                // Chuyển đổi dữ liệu từ Jacred sang định dạng Lampa hiểu
+                var obj = {
+                    title: item.title,
+                    quality: item.quality || '720p',
+                    size: Lampa.Utils.bytesToSize(item.size),
+                    seeders: item.seeds,
+                    leechers: item.peers,
+                    magnet: item.magnet,
+                    plugin: 'Jacred Mod by Ni'
+                };
+
+                var card = Lampa.Template.get('torrent_item', obj);
+                
+                card.on('hover:enter', function () {
+                    // Gọi trình phát Torrent (TorrServe/WebTorr)
+                    Lampa.Torrent.run(obj);
+                });
+
+                body.append(card);
+                items.push(card);
+            });
+
+            this.onReatdy();
+        };
+
+        this.empty = function () {
+            body.append('<div class="empty">Không tìm thấy kết quả từ Jacred</div>');
+            this.onReatdy();
+        };
+
+        this.render = function () {
+            return html;
+        };
+
+        this.onReatdy = function () {
+            html.append(scroll.render());
+            scroll.append(body);
+        };
     }
 
-    if (window.appready) TorrentioJackettStyle();
-    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') TorrentioJackettStyle(); });
+    // Đăng ký plugin vào hệ thống Lampa
+    if (window.lampa_plugins) {
+        Lampa.Component.add('jackett', Jackett);
+        Lampa.Search.addSource(Jackett);
+    }
 })();
