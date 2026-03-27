14cd2c54f607e9703a651433b32a7e5938842af8
nguyenquocngu93 <158430142+nguyenquocngu93@users.noreply.github.com>
2026-03-27 18:02:14 +0700

    Update kk2.js

--- kk2.js
+++ kk2.js
@@ -2,7 +2,7 @@
     'use strict';
 
     function kkphimPlugin() {
-        // 1. Thêm mục vào menu bên trái
+        // 1. Tạo Component hiển thị nội dung khi bấm vào menu
         Lampa.Component.add('kkphim_component', function (object) {
             var network = new Lampa.Reguest();
             var scroll  = new Lampa.Scroll({mask: true, over: true});
@@ -11,14 +11,11 @@
             
             this.create = function () {
                 var _this = this;
-                
-                // Giao diện khi nhấn vào menu KKPhim
                 html.append(scroll.render());
                 
-                // Demo: Hiển thị dòng chữ thông báo
-                var info = $('<div class="category-full"><h2>Chào mừng đến với KKPhim</h2><p>Dữ liệu đang được tải...</p></div>');
+                // Hiển thị nội dung tạm thời
+                var info = $('<div class="category-full" style="padding: 20px; text-align: center;"><h2>KKPhim</h2><p>Dữ liệu sẽ hiển thị tại đây...</p></div>');
                 scroll.append(info);
-                
                 return this.render();
             };
 
@@ -27,34 +24,44 @@
             };
         });
 
-        // 2. Đăng ký Menu item
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') {
-                var menu_item = $(`
-                    <div class="menu__item selector" data-action="kkphim">
-                        <svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="white"/>
+        // 2. Hàm chèn Menu vào sidebar
+        function addMenuItem() {
+            var menu_item = $(`
+                <div class="menu__item selector" data-action="kkphim">
+                    <div class="menu__ico">
+                        <svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM10 16.5V7.5L16 12L10 16.5Z" fill="white"/>
                         </svg>
-                        <div class="menu__text">KKPhim</div>
                     </div>
-                `);
+                    <div class="menu__text">KKPhim</div>
+                </div>
+            `);
 
-                menu_item.on('hover:enter', function () {
-                    Lampa.Activity.push({
-                        url: '',
-                        title: 'KKPhim',
-                        component: 'kkphim_component',
-                        page: 1
-                    });
+            menu_item.on('hover:enter', function () {
+                Lampa.Activity.push({
+                    url: '',
+                    title: 'KKPhim',
+                    component: 'kkphim_component',
+                    page: 1
                 });
+                // Đóng menu sau khi chọn (trên mobile)
+                Lampa.Menu.hide();
+            });
 
-                // Chèn vào menu bên trái (thường là sau mục "Phim")
-                $('.menu .menu__list').append(menu_item);
-            }
-        });
+            // Chèn vào cuối danh sách menu
+            $('.menu .menu__list').append(menu_item);
+        }
+
+        // Đợi app sẵn sàng thì chèn menu
+        if (window.appready) addMenuItem();
+        else {
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type == 'ready') addMenuItem();
+            });
+        }
     }
 
-    // Khởi chạy plugin
+    // Khởi chạy script
     if (window.appready) kkphimPlugin();
     else {
         Lampa.Listener.follow('app', function (e) {
