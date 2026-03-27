b350b26ceee765868f372e7629a019b058292ec4
nguyenquocngu93 <158430142+nguyenquocngu93@users.noreply.github.com>
2026-03-27 12:25:16 +0700

    Update kk2.js

--- kk2.js
+++ kk2.js
@@ -1,12 +1,15 @@
 /*
-KKPhim Plugin for Lampa
-- Trang chủ: row ngang category
-- Xem thêm: infinite scroll
-- Chọn tập ngay trong UI
+KKPHIM FULL PLUGIN FOR LAMPA
+- Fix menu
+- Home row ngang
+- Xem thêm → infinite scroll
+- Chọn tập ngay UI
 */
 
 (function () {
 'use strict';
+
+console.log('✅ KKPHIM FULL LOADED');
 
 var API = 'https://phimapi.com';
 var IMG = 'https://phimimg.com/';
@@ -18,257 +21,288 @@ function fixImg(url){
 
 function startPlugin() {
 
-    /**
-     * =========================
-     * HOME (ROW NGANG)
-     * =========================
-     */
-    Lampa.Component.add('kkphim_home', {
-        render: function () {
-            this.create();
-            this.load();
-        },
+    console.log('🚀 START KKPHIM');
 
-        create: function () {
-            this.html = $('<div class="kk-home"></div>');
-            this.activity.append(this.html);
-            this.activity.loader(true);
-        },
+    try {
 
-        load: function () {
-            var _this = this;
+        /**
+         * =========================
+         * HOME (ROW NGANG)
+         * =========================
+         */
+        Lampa.Component.add('kkphim_home', {
+            render: function () {
+                console.log('📺 HOME RENDER');
 
-            var categories = [
-                {title: 'Phim mới', type: 'phim-moi-cap-nhat'},
-                {title: 'Phim bộ', type: 'phim-bo'},
-                {title: 'Phim lẻ', type: 'phim-le'},
-                {title: 'Hoạt hình', type: 'hoat-hinh'}
-            ];
+                this.create();
+                this.load();
+            },
 
-            this.activity.loader(false);
+            create: function () {
+                this.html = $('<div class="kk-home"></div>');
+                this.activity.append(this.html);
+                this.activity.loader(true);
+            },
 
-            categories.forEach(function (cat) {
-                _this.renderRow(cat);
-            });
-        },
+            load: function () {
+                var _this = this;
 
-        renderRow: function (cat) {
-            var _this = this;
+                var categories = [
+                    {title: 'Phim mới', type: 'phim-moi-cap-nhat'},
+                    {title: 'Phim bộ', type: 'phim-bo'},
+                    {title: 'Phim lẻ', type: 'phim-le'},
+                    {title: 'Hoạt hình', type: 'hoat-hinh'}
+                ];
 
-            var row = $(`
-                <div class="kk-row">
-                    <div class="kk-row-head">
-                        <div class="kk-title">${cat.title}</div>
-                        <div class="kk-more">Xem thêm</div>
+                this.activity.loader(false);
+
+                categories.forEach(function (cat) {
+                    _this.renderRow(cat);
+                });
+            },
+
+            renderRow: function (cat) {
+                var _this = this;
+
+                var row = $(`
+                    <div class="kk-row">
+                        <div class="kk-head">
+                            <div class="kk-title">${cat.title}</div>
+                            <div class="kk-more">Xem thêm</div>
+                        </div>
+                        <div class="kk-list"></div>
                     </div>
-                    <div class="kk-list"></div>
-                </div>
-            `);
+                `);
 
-            this.html.append(row);
+                this.html.append(row);
 
-            // click xem thêm
-            row.find('.kk-more').on('hover:enter', function () {
-                Lampa.Activity.push({
-                    url: '',
-                    component: 'kkphim_list',
-                    params: { type: cat.type, title: cat.title }
-                });
-            });
-
-            // load data row
-            Lampa.Reguest.get({
-                url: API + '/danh-sach/' + cat.type + '?page=1',
-                success: function (res) {
-
-                    var list = row.find('.kk-list');
-
-                    res.items.slice(0,10).forEach(function (item) {
-
-                        var card = Lampa.Template.get('card', {
-                            title: item.name,
-                            img: fixImg(item.poster_url || item.thumb_url),
-                            subtitle: item.episode_current
-                        });
-
-                        $(card).on('hover:enter', function () {
-                            _this.open(item);
-                        });
-
-                        list.append(card);
+                row.find('.kk-more').on('hover:enter', function () {
+                    Lampa.Activity.push({
+                        component: 'kkphim_list',
+                        params: { type: cat.type, title: cat.title }
                     });
-                }
-            });
-        },
+                });
 
-        open: function (item) {
-            var _this = this;
+                Lampa.Reguest.get({
+                    url: API + '/danh-sach/' + cat.type + '?page=1',
+                    success: function (res) {
 
-            Lampa.Reguest.get({
-                url: API + '/phim/' + item.slug,
-                success: function (res) {
-                    _this.showEpisodes(item, res.episodes);
-                }
-            });
-        },
+                        var list = row.find('.kk-list');
 
-        showEpisodes: function (item, episodes) {
-            var _this = this;
+                        res.items.slice(0,10).forEach(function (item) {
 
-            this.html.empty();
+                            var card = Lampa.Template.get('card', {
+                                title: item.name,
+                                img: fixImg(item.poster_url || item.thumb_url),
+                                subtitle: item.episode_current
+                            });
 
-            episodes.forEach(function (server) {
+                            $(card).on('hover:enter', function () {
+                                _this.open(item);
+                            });
 
-                var title = $('<div class="kk-server">' + server.server_name + '</div>');
-                var wrap = $('<div class="kk-episodes"></div>');
-
-                server.server_data.forEach(function (ep) {
-
-                    var btn = $('<div class="kk-ep">' + ep.name + '</div>');
-
-                    btn.on('hover:enter', function () {
-                        Lampa.Player.play({
-                            url: ep.link_m3u8 || ep.link_embed,
-                            title: item.name + ' - ' + ep.name
+                            list.append(card);
                         });
-                    });
-
-                    wrap.append(btn);
+                    }
                 });
+            },
 
-                _this.html.append(title);
-                _this.html.append(wrap);
-            });
-        }
-    });
+            open: function (item) {
+                var _this = this;
 
+                console.log('🎬 OPEN:', item.name);
 
-    /**
-     * =========================
-     * LIST (INFINITE SCROLL)
-     * =========================
-     */
-    Lampa.Component.add('kkphim_list', {
-        render: function () {
-            this.create();
-            this.load();
-        },
-
-        create: function () {
-            var _this = this;
-
-            this.page = 1;
-            this.loading = false;
-
-            this.html = $('<div class="kk-list-page"></div>');
-            this.activity.append(this.html);
-
-            this.scroll = new Lampa.Scroll({
-                mask: true,
-                over: true
-            });
-
-            this.scroll.render().append(this.html);
-
-            this.scroll.on('end', function () {
-                _this.load();
-            });
-        },
-
-        load: function () {
-            var _this = this;
-
-            if (this.loading) return;
-            this.loading = true;
-
-            Lampa.Reguest.get({
-                url: API + '/danh-sach/' + this.params.type + '?page=' + this.page,
-                success: function (res) {
-
-                    _this.build(res.items);
-
-                    _this.page++;
-                    _this.loading = false;
-                }
-            });
-        },
-
-        build: function (items) {
-            var _this = this;
-
-            items.forEach(function (item) {
-
-                var card = Lampa.Template.get('card', {
-                    title: item.name,
-                    img: fixImg(item.poster_url || item.thumb_url),
-                    subtitle: item.episode_current
+                Lampa.Reguest.get({
+                    url: API + '/phim/' + item.slug,
+                    success: function (res) {
+                        _this.showEpisodes(item, res.episodes);
+                    }
                 });
+            },
 
-                $(card).on('hover:enter', function () {
-                    _this.open(item);
-                });
+            showEpisodes: function (item, episodes) {
+                var _this = this;
 
-                _this.html.append(card);
-            });
-        },
+                this.html.empty();
 
-        open: function (item) {
-            var _this = this;
+                episodes.forEach(function (server) {
 
-            Lampa.Reguest.get({
-                url: API + '/phim/' + item.slug,
-                success: function (res) {
-                    _this.showEpisodes(item, res.episodes);
-                }
-            });
-        },
+                    var title = $('<div class="kk-server">' + server.server_name + '</div>');
+                    var wrap = $('<div class="kk-episodes"></div>');
 
-        showEpisodes: function (item, episodes) {
-            var _this = this;
+                    server.server_data.forEach(function (ep) {
 
-            this.html.empty();
+                        var btn = $('<div class="kk-ep">' + ep.name + '</div>');
 
-            episodes.forEach(function (server) {
+                        btn.on('hover:enter', function () {
+                            console.log('▶ PLAY:', ep.name);
 
-                var title = $('<div class="kk-server">' + server.server_name + '</div>');
-                var wrap = $('<div class="kk-episodes"></div>');
-
-                server.server_data.forEach(function (ep) {
-
-                    var btn = $('<div class="kk-ep">' + ep.name + '</div>');
-
-                    btn.on('hover:enter', function () {
-                        Lampa.Player.play({
-                            url: ep.link_m3u8 || ep.link_embed,
-                            title: item.name + ' - ' + ep.name
+                            Lampa.Player.play({
+                                url: ep.link_m3u8 || ep.link_embed,
+                                title: item.name + ' - ' + ep.name
+                            });
                         });
+
+                        wrap.append(btn);
                     });
 
-                    wrap.append(btn);
+                    _this.html.append(title);
+                    _this.html.append(wrap);
+                });
+            }
+        });
+
+
+        /**
+         * =========================
+         * LIST (INFINITE SCROLL)
+         * =========================
+         */
+        Lampa.Component.add('kkphim_list', {
+            render: function () {
+                console.log('📃 LIST RENDER');
+
+                this.create();
+                this.load();
+            },
+
+            create: function () {
+                var _this = this;
+
+                this.page = 1;
+                this.loading = false;
+
+                this.html = $('<div class="kk-list-page"></div>');
+                this.activity.append(this.html);
+
+                this.scroll = new Lampa.Scroll({
+                    mask: true,
+                    over: true
                 });
 
-                _this.html.append(title);
-                _this.html.append(wrap);
-            });
-        }
-    });
+                this.scroll.render().append(this.html);
+
+                this.scroll.on('end', function () {
+                    _this.load();
+                });
+            },
+
+            load: function () {
+                var _this = this;
+
+                if (this.loading) return;
+                this.loading = true;
+
+                console.log('📥 LOAD PAGE:', this.page);
+
+                Lampa.Reguest.get({
+                    url: API + '/danh-sach/' + this.params.type + '?page=' + this.page,
+                    success: function (res) {
+
+                        _this.build(res.items);
+
+                        _this.page++;
+                        _this.loading = false;
+                    }
+                });
+            },
+
+            build: function (items) {
+                var _this = this;
+
+                items.forEach(function (item) {
+
+                    var card = Lampa.Template.get('card', {
+                        title: item.name,
+                        img: fixImg(item.poster_url || item.thumb_url),
+                        subtitle: item.episode_current
+                    });
+
+                    $(card).on('hover:enter', function () {
+                        _this.open(item);
+                    });
+
+                    _this.html.append(card);
+                });
+            },
+
+            open: function (item) {
+                var _this = this;
+
+                Lampa.Reguest.get({
+                    url: API + '/phim/' + item.slug,
+                    success: function (res) {
+                        _this.showEpisodes(item, res.episodes);
+                    }
+                });
+            },
+
+            showEpisodes: function (item, episodes) {
+                var _this = this;
+
+                this.html.empty();
+
+                episodes.forEach(function (server) {
+
+                    var title = $('<div class="kk-server">' + server.server_name + '</div>');
+                    var wrap = $('<div class="kk-episodes"></div>');
+
+                    server.server_data.forEach(function (ep) {
+
+                        var btn = $('<div class="kk-ep">' + ep.name + '</div>');
+
+                        btn.on('hover:enter', function () {
+                            Lampa.Player.play({
+                                url: ep.link_m3u8 || ep.link_embed,
+                                title: item.name + ' - ' + ep.name
+                            });
+                        });
+
+                        wrap.append(btn);
+                    });
+
+                    _this.html.append(title);
+                    _this.html.append(wrap);
+                });
+            }
+        });
 
 
-    /**
-     * =========================
-     * MENU
-     * =========================
-     */
-    Lampa.Menu.add({
-        title: 'KKPhim',
-        component: 'kkphim_home',
-        icon: 'movie'
-    });
+        /**
+         * =========================
+         * MENU (FIX CHẮC CHẮN HIỆN)
+         * =========================
+         */
+        Lampa.Menu.add({
+            title: 'KKPhim',
+            component: 'kkphim_home',
+            icon: 'movie',
+            id: 'kkphim_menu'
+        });
+
+        console.log('✅ MENU ADDED OK');
+
+    } catch (e) {
+        console.error('❌ KKPHIM ERROR:', e);
+    }
 }
 
-if (window.appready) startPlugin();
-else Lampa.Listener.follow('app', function (e) {
-    if (e.type == 'ready') startPlugin();
-});
+/**
+ * =========================
+ * APP READY FIX
+ * =========================
+ */
+if (window.appready) {
+    console.log('⚡ APP READY TRUE');
+    startPlugin();
+} else {
+    console.log('⏳ WAIT APP READY...');
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type === 'ready') {
+            console.log('⚡ APP READY EVENT');
+            startPlugin();
+        }
+    });
+}
 
 })();
