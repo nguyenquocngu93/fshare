08ea715ddf0b19e67e70d4b08ea73ed62c834b23
nguyenquocngu93 <158430142+nguyenquocngu93@users.noreply.github.com>
2026-03-28 12:08:38 +0700

    Update kk2.js

--- kk2.js
+++ kk2.js
@@ -1,5 +1,5 @@
 (function () {
-    // Polyfills (giữ nguyên từ plugin mẫu)
+    // Polyfills (giữ nguyên)
     if (!Object.keys) { Object.keys = function getObjectKeys(o) { var r = [], k; for (k in o) { if (Object.prototype.hasOwnProperty.call(o, k)) { r.push(k); } } return r; }; }
     if (!Array.prototype.map) { Array.prototype.map = function mapArray(c, t) { if (this == null) { throw new TypeError('Array is null or undefined'); } var s = Object(this), l = s.length >>> 0; if (typeof c !== 'function') { throw new TypeError(c + ' is not a function'); } var r = new Array(l); for (var i = 0; i < l; i++) { if (i in s) { r[i] = c.call(t, s[i], i, s); } } return r; }; }
     if (!Array.prototype.forEach) { Array.prototype.forEach = function forEachArray(c, t) { if (this == null) { throw new TypeError('Array is null or undefined'); } var s = Object(this), l = s.length >>> 0; if (typeof c !== 'function') { throw new TypeError(c + ' is not a function'); } for (var i = 0; i < l; i++) { if (i in s) { c.call(t, s[i], i, s); } } }; }
@@ -11,49 +11,32 @@
 
     var SOURCE_NAME = 'KPHim';
     var CACHE_SIZE = 100;
-    var CACHE_TIME = 1000 * 60 * 60 * 3; // 3 giờ
+    var CACHE_TIME = 1000 * 60 * 60 * 3;
     var cache = {};
 
-    // Cấu hình API
     var API_BASE_URL = 'https://phimapi.com';
     var IMAGE_BASE_URL = 'https://phimimg.com';
-    
-    // Các danh mục
+
     var CATEGORIES = {
-        phim_bo: 'phim-bo',
-        phim_le: 'phim-le',
-        tv_show: 'tv-show',
-        hoathinh: 'hoathinh',
-        phim_chieu_rap: 'phim-chieu-rap'
+        'phim-bo': 'Phim bộ',
+        'phim-le': 'Phim lẻ',
+        'tv-show': 'TV Shows',
+        'hoathinh': 'Hoạt hình',
+        'phim-chieu-rap': 'Phim chiếu rạp'
     };
 
-    var DISPLAY_OPTIONS = {
-        phim_bo: {
-            title: 'Phim bộ',
+    var DISPLAY_OPTIONS = {};
+    for (var cat in CATEGORIES) {
+        DISPLAY_OPTIONS[cat] = {
+            title: CATEGORIES[cat],
             visible: true
-        },
-        phim_le: {
-            title: 'Phim lẻ',
-            visible: true
-        },
-        tv_show: {
-            title: 'TV Shows',
-            visible: true
-        },
-        hoathinh: {
-            title: 'Hoạt hình',
-            visible: true
-        },
-        phim_chieu_rap: {
-            title: 'Phim chiếu rạp',
-            visible: true
-        }
-    };
+        };
+    }
 
     function KPHimApiService() {
         var self = this;
         self.network = new Lampa.Reguest();
-        
+
         function getCache(key) {
             var res = cache[key];
             if (res) {
@@ -97,81 +80,73 @@
             };
         }
 
-        function normalizeData(json, type) {
-            var items = json.items || json.data || [];
-            if (!Array.isArray(items) && items.results) items = items.results;
-            
+        function normalizeData(json) {
+            var items = [];
+            if (json && json.data && json.data.items) {
+                items = json.data.items;
+            } else if (json && json.items) {
+                items = json.items;
+            } else if (Array.isArray(json)) {
+                items = json;
+            }
+
             return {
-                results: (items || []).map(function (item) {
-                    var dataItem = {
-                        id: item._id || item.id,
+                results: items.map(function (item) {
+                    return {
+                        id: item._id,
                         name: item.name,
                         title: item.name,
-                        original_name: item.origin_name || item.name,
-                        poster_path: item.poster_url || item.thumb_url || '',
-                        img: item.poster_url || item.thumb_url || '',
-                        overview: item.content || item.description || '',
+                        original_name: item.origin_name,
+                        poster_path: item.poster_url || item.thumb_url,
+                        img: item.poster_url || item.thumb_url,
+                        overview: item.content || item.description,
                         vote_average: item.tmdb?.vote_average || 0,
-                        year: item.year || (item.premiered ? item.premiered.split('-')[0] : ''),
-                        type: item.type || type
+                        year: item.year,
+                        slug: item.slug,
+                        source: SOURCE_NAME
                     };
-                    
-                    if (item.slug) dataItem.slug = item.slug;
-                    if (item.status) dataItem.status = item.status;
-                    
-                    dataItem.promo_title = dataItem.name;
-                    dataItem.promo = dataItem.overview;
-                    dataItem.source = SOURCE_NAME;
-                    
-                    return dataItem;
                 }),
                 page: json.page || 1,
-                total_pages: Math.ceil((json.total || json.params?.pagination?.totalItems || 0) / 20) || 1,
-                total_results: json.total || json.params?.pagination?.totalItems || 0
+                total_pages: Math.ceil((json.data?.total || json.total || 0) / 20) || 1,
+                total_results: json.data?.total || json.total || 0
             };
         }
 
-        self.get = function (url, params, onComplete, onError) {
-            self.network.silent(url, function (json) {
-                if (!json) {
-                    onError(new Error('Empty response from server'));
-                    return;
-                }
-                var normalizedJson = normalizeData(json);
-                setCache(url, normalizedJson);
-                onComplete(normalizedJson);
-            }, function (error) {
-                onError(error);
-            });
-        };
-
-        self.list = function (params, onComplete, onError) {
-            params = params || {};
-            onComplete = onComplete || function () { };
-            onError = onError || function () { };
-
-            var category = params.url || 'phim-bo';
-            var page = params.page || 1;
-            var limit = 20;
-            
-            var url = API_BASE_URL + '/v1/api/danh-sach/' + category + '?page=' + page + '&limit=' + limit;
-            
+        self.get = function (url, onComplete, onError) {
             var cached = getCache(url);
             if (cached) {
                 onComplete(cached);
                 return;
             }
-            
-            self.get(url, params, onComplete, onError);
+
+            self.network.silent(url, function (json) {
+                if (!json) {
+                    onError(new Error('Empty response'));
+                    return;
+                }
+                var normalized = normalizeData(json);
+                setCache(url, normalized);
+                onComplete(normalized);
+            }, function (err) {
+                onError(err);
+            });
+        };
+
+        self.list = function (params, onComplete, onError) {
+            params = params || {};
+            var category = params.url || 'phim-bo';
+            var page = params.page || 1;
+            var limit = 20;
+            var url = API_BASE_URL + '/v1/api/danh-sach/' + category + '?page=' + page + '&limit=' + limit;
+            self.get(url, onComplete, onError);
         };
 
         self.full = function (params, onSuccess, onError) {
             var slug = params.card.slug;
             if (!slug) {
-                onError(new Error('No slug provided'));
+                onError(new Error('No slug'));
                 return;
             }
-            
             var url = API_BASE_URL + '/v1/api/phim/' + slug;
             self.network.silent(url, function (json) {
                 if (json && json.data && json.data.movie) {
@@ -198,55 +173,35 @@
         };
 
         self.category = function (params, onSuccess, onError) {
-            params = params || {};
             var partsData = [];
-            
-            Object.keys(DISPLAY_OPTIONS).forEach(function(key) {
+            for (var key in DISPLAY_OPTIONS) {
                 if (DISPLAY_OPTIONS[key].visible) {
-                    partsData.push(function(callback) {
-                        callback({
-                            source: SOURCE_NAME,
-                            url: CATEGORIES[key],
-                            title: DISPLAY_OPTIONS[key].title,
-                            page: 1,
-                            more: true,
-                            results: []
-                        });
-                    });
+                    partsData.push((function(catKey, catTitle) {
+                        return function(callback) {
+                            callback({
+                                source: SOURCE_NAME,
+                                url: catKey,
+                                title: catTitle,
+                                page: 1,
+                                more: true,
+                                results: []
+                            });
+                        };
+                    })(key, DISPLAY_OPTIONS[key].title));
                 }
-            });
-            
-            function loadPart(partLoaded, partEmpty) {
-                Lampa.Api.partNext(partsData, 5, function(result) {
-                    partLoaded(result);
-                }, function(error) {
-                    partEmpty(error);
-                });
             }
-            
-            loadPart(onSuccess, onError);
+            Lampa.Api.partNext(partsData, 5, onSuccess, onError);
         };
 
         self.search = function (params, onSuccess, onError) {
-            params = params || {};
             var query = params.query;
             var page = params.page || 1;
-            
             if (!query) {
-                onError(new Error('No search query'));
+                onError(new Error('No query'));
                 return;
             }
-            
             var url = API_BASE_URL + '/v1/api/tim-kiem?keyword=' + encodeURIComponent(query) + '&page=' + page;
-            
-            self.network.silent(url, function(json) {
-                if (json && json.data && json.data.items) {
-                    var normalized = normalizeData({ items: json.data.items, page: page, total: json.data.total });
-                    onSuccess(normalized);
-                } else {
-                    onSuccess({ results: [], page: 1, total_pages: 1, total_results: 0 });
-                }
-            }, onError);
+            self.get(url, onSuccess, onError);
         };
 
         self.clear = function () {
@@ -284,9 +239,7 @@
     }
 
     function startPlugin() {
-        if (window.kphim_plugin) {
-            return;
-        }
+        if (window.kphim_plugin) return;
         window.kphim_plugin = true;
 
         var CAT_NAME = Lampa.Storage.get('kphim_settings_cat_name', SOURCE_NAME);
@@ -330,39 +283,38 @@
             }
         });
 
-        Object.keys(DISPLAY_OPTIONS).forEach(function(option) {
-            var settingName = 'kphim_settings_' + option + '_visible';
-            var visible = Lampa.Storage.get(settingName, "true").toString() === "true";
-            DISPLAY_OPTIONS[option].visible = visible;
+        for (var key in DISPLAY_OPTIONS) {
+            (function(catKey, catTitle) {
+                var settingName = 'kphim_settings_' + catKey + '_visible';
+                var visible = Lampa.Storage.get(settingName, "true").toString() === "true";
+                DISPLAY_OPTIONS[catKey].visible = visible;
 
-            Lampa.SettingsApi.addParam({
-                component: "kphim_settings",
-                param: {
-                    name: settingName,
-                    type: "trigger",
-                    default: visible
-                },
-                field: {
-                    name: DISPLAY_OPTIONS[option].title,
-                    description: 'Hiển thị danh mục này'
-                },
-                onChange: function(value) {
-                    DISPLAY_OPTIONS[option].visible = value === "true";
-                }
-            });
-        });
+                Lampa.SettingsApi.addParam({
+                    component: "kphim_settings",
+                    param: {
+                        name: settingName,
+                        type: "trigger",
+                        default: visible
+                    },
+                    field: {
+                        name: catTitle,
+                        description: 'Hiển thị danh mục này'
+                    },
+                    onChange: function(value) {
+                        DISPLAY_OPTIONS[catKey].visible = value === "true";
+                    }
+                });
+            })(key, DISPLAY_OPTIONS[key].title);
+        }
 
         var kphimApi = new KPHimApiService();
         Lampa.Api.sources.kphim = kphimApi;
         Object.defineProperty(Lampa.Api.sources, SOURCE_NAME, {
-            get: function () {
-                return kphimApi;
-            }
+            get: function () { return kphimApi; }
         });
 
         var menuItem = $('<li data-action="kphim" class="menu__item selector"><div class="menu__ico">' + ICON + '</div><div class="menu__text kphim_cat_text">' + CAT_NAME + '</div></li>');
         $('.menu .menu__list').eq(0).append(menuItem);
-
         menuItem.on('hover:enter', function () {
             Lampa.Activity.push({
                 title: CAT_NAME,
