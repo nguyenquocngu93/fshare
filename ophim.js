010ff1fb30ddeef8aa518d8590b0256f67aacc5e
nguyenquocngu93 <158430142+nguyenquocngu93@users.noreply.github.com>
2026-04-01 19:55:50 +0700

    Update ophim.js

--- ophim.js
+++ ophim.js
@@ -1,35 +1,44 @@
+/*
+@name KK Plugin
+@version 1.0.0
+@author NguyenQuocNgu
+@description Loader for kkcore.js and kkui.js
+*/
+
 (function() {
   if (window.__kkplugin_loader__) return;
   window.__kkplugin_loader__ = true;
 
-  function loadText(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200 || xhr.status === 0) {
-          callback(null, xhr.responseText);
-        } else {
-          callback(new Error('Không tải được: ' + url));
-        }
-      }
+  var scripts = [
+    'https://nguyenquocngu93.github.io/fshare/kkcore.js',
+    'https://nguyenquocngu93.github.io/fshare/kkui.js'
+  ];
+
+  function loadScript(index) {
+    if (index >= scripts.length) {
+      console.log('KK Plugin: load xong toàn bộ');
+      return;
+    }
+
+    var src = scripts[index];
+
+    if (document.querySelector('script[src="' + src + '"]')) {
+      loadScript(index + 1);
+      return;
+    }
+
+    var script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    script.onload = function() {
+      loadScript(index + 1);
     };
-    xhr.send();
+    script.onerror = function() {
+      console.error('KK Plugin: không load được ' + src);
+    };
+
+    document.head.appendChild(script);
   }
 
-  loadText('https://nguyenquocngu93.github.io/fshare/kkcore.js', function(err, core) {
-    if (err) return console.error(err);
-
-    loadText('https://nguyenquocngu93.github.io/fshare/kkui.js', function(err2, ui) {
-      if (err2) return console.error(err2);
-
-      try {
-        eval(core + '\n//# sourceURL=kkcore.js');
-        eval(ui + '\n//# sourceURL=kkui.js');
-        console.log('KK Plugin: load xong toàn bộ');
-      } catch (e) {
-        console.error('KK Plugin: lỗi khi thực thi', e);
-      }
-    });
-  });
+  loadScript(0);
 })();
